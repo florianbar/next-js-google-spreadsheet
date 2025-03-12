3c410ebb3d583b551d8e074a021981177529c6da
Florian Bär <florianbar01@gmail.com>
2025-03-12 15:12:56 +0200

    updates to get and post methods

--- app/api/meals/route.ts
+++ app/api/meals/route.ts
@@ -2,68 +2,86 @@ import { sheets_v4 } from 'googleapis';
 import { GaxiosResponse } from 'gaxios';
 
 import { getAuth } from '@/utils/auth';
+import { getTodayISOString } from '@/utils/date';
 
 const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
 const SHEET_NAME = process.env.SHEET_NAME;
 
 export async function GET() {
-    const { auth, googleSheets } = await getAuth();
+    try {
+        const { auth, googleSheets } = await getAuth();
 
-    // // Get metadata about the spreadsheet
-    // const metaData: GaxiosResponse<sheets_v4.Schema$Spreadsheet> = await googleSheets.spreadsheets.get({
-    //     auth,
-    //     spreadsheetId,
-    // });
+        // // Get metadata about the spreadsheet
+        // const metaData: GaxiosResponse<sheets_v4.Schema$Spreadsheet> = await googleSheets.spreadsheets.get({
+        //     auth,
+        //     spreadsheetId,
+        // });
 
-    // Read rows from the spreadsheet
-    const rows: GaxiosResponse<sheets_v4.Schema$ValueRange> = await googleSheets.spreadsheets.values.get({
-        auth,
-        spreadsheetId: SPREADSHEET_ID,
-        range: `${SHEET_NAME}!A:D`, // can provide column number
-    });
+        // Read rows from the spreadsheet
+        const response: GaxiosResponse<sheets_v4.Schema$ValueRange> = await googleSheets.spreadsheets.values.get({
+            auth,
+            spreadsheetId: SPREADSHEET_ID,
+            range: `${SHEET_NAME}!A:D`, // can provide column number
+        });
 
-    return new Response(JSON.stringify(rows.data), {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' }
-    });
+        if (response.status !== 200) {
+            // throw error
+        }
+
+        return new Response(JSON.stringify(response.data), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+    } catch (error) {
+        return new Response(JSON.stringify(error), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
 }
 
-export async function POST() {
-    const { auth, googleSheets } = await getAuth();
+export async function POST(request: Request) {
+    try {
+        // Parse the request body as JSON
+        const requestBody = await request.json();
 
-    const now = new Date();
-    const isoDateTime = now.toISOString().slice(0, 19); 
-    console.log("isoDateTime ::::", isoDateTime);
+        if (!requestBody.food || !requestBody.quantity || !requestBody.healthy || !requestBody.date) {
+            return new Response(JSON.stringify({ error: "Missing required fields" }), {
+                status: 400,
+                headers: { 'Content-Type': 'application/json' }
+            });
+        }
 
-    // Write row(s) to the spreadsheet
-    await googleSheets.spreadsheets.values.append({
-        auth,
-        spreadsheetId: SPREADSHEET_ID,
-        range: `${SHEET_NAME}!A:D`,
-        valueInputOption: "USER_ENTERED", // "RAW" or "USER_ENTERED"
-        resource: {
-            values: [
-                [isoDateTime, "Egg", 1, true],
-            ],
-        },
-    });
+        const { auth, googleSheets } = await getAuth();
+        const date = requestBody.date ?? getTodayISOString();
 
-    return new Response("", {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' }
-    });
+        // Write row(s) to the spreadsheet
+        const response = await googleSheets.spreadsheets.values.append({
+            auth,
+            spreadsheetId: SPREADSHEET_ID,
+            range: `${SHEET_NAME}!A:D`,
+            valueInputOption: "USER_ENTERED", // "RAW" or "USER_ENTERED"
+            resource: {
+                values: [
+                    [date, requestBody.food, requestBody.quantity, requestBody.healthy],
+                ],
+            },
+        });
+
+        if (response.status !== 200) {
+            // throw error
+        }
+
+        return new Response(JSON.stringify(response), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+    } catch (error) {
+        return new Response(JSON.stringify(error), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
 }
- 
-// export async function POST(request: Request) {
-//   // Parse the request body
-//   const body = await request.json();
-//   const { name } = body;
- 
-//   // e.g. Insert new user into your DB
-//   const newUser = { id: Date.now(), name };
- 
-//   return new Response(JSON.stringify(newUser), {
-//     status: 201,
-//     headers: { 'Content-Type': 'application/json' }
-//   });
-// }
