de998febb0bff98d7cb289680d78587df1ed5052
Florian Bär <florianbar01@gmail.com>
2025-04-07 14:01:29 +0200

    add size variation to button

--- client/components/ui/buttons/button.tsx
+++ client/components/ui/buttons/button.tsx
@@ -6,22 +6,36 @@ interface ButtonProps {
   children: string | JSX.Element;
   onPress: () => void;
   disabled?: boolean;
+  size?: "sm" | "md" | "lg";
 }
 
-function Button({ children, onPress, disabled }: ButtonProps) {
+function Button({ children, onPress, disabled, size = "md" }: ButtonProps) {
+  const buttonInnerContainerStyles: object[] = [styles.buttonInnerContainer];
+  const buttonTextStyles: object[] = [styles.buttonText];
+
+  switch (size) {
+    case "sm":
+      buttonInnerContainerStyles.push(styles.buttonInnerContainerSm);
+      buttonTextStyles.push(styles.buttonTextSm);
+      break;
+    case "lg":
+      buttonInnerContainerStyles.push(styles.buttonInnerContainerLg);
+      buttonTextStyles.push(styles.buttonTextLg);
+  }
+
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
         style={({ pressed }: { pressed: boolean }) =>
           pressed
-            ? [styles.buttonInnerContainer, styles.pressed]
-            : styles.buttonInnerContainer
+            ? [...buttonInnerContainerStyles, styles.pressed]
+            : buttonInnerContainerStyles
         }
         onPress={onPress}
         android_ripple={{ color: COLORS.greenDark }}
         disabled={disabled}
       >
-        <Text style={styles.buttonText}>{children}</Text>
+        <Text style={buttonTextStyles}>{children}</Text>
       </Pressable>
     </View>
   );
@@ -31,21 +45,35 @@ export default Button;
 
 const styles = StyleSheet.create({
   buttonOuterContainer: {
-    borderRadius: 8,
+    borderRadius: 40,
     margin: 4,
     overflow: "hidden",
   },
   buttonInnerContainer: {
-    backgroundColor: COLORS.green,
+    backgroundColor: COLORS.blue,
     paddingVertical: 8,
-    paddingHorizontal: 12,
+    paddingHorizontal: 16,
     elevation: 2,
+  },
+  buttonInnerContainerSm: {
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+  },
+  buttonInnerContainerLg: {
+    paddingVertical: 12,
+    paddingHorizontal: 20,
   },
   buttonText: {
     color: "white",
     fontSize: 16,
     fontWeight: "bold",
     textAlign: "center",
+  },
+  buttonTextSm: {
+    fontSize: 12,
+  },
+  buttonTextLg: {
+    fontSize: 20,
   },
   pressed: {
     ...Platform.select({
