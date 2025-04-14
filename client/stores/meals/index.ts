965712e22b17b39cf0682cfdfe781e49e1b19cc0
Florian Bär <florianbar01@gmail.com>
2025-04-14 09:06:57 +0200

    clean-up store

--- client/stores/meals/index.ts
+++ client/stores/meals/index.ts
@@ -1,47 +1,21 @@
 import { create } from "zustand";
 
-import { MealUI } from "../../types/meals";
-import { MealStoreState, ActionProps } from "./types";
-import { api } from "../../utils/api";
+import { MealStoreState } from "./types";
 
 const initialState = {
-  selectedDate: null,
-  meals: [],
-  pendingMeals: [],
-  loading: false,
-  error: null,
+  selectedDate: new Date().toISOString().split("T")[0],
 };
 
 const useMealsStore = create<MealStoreState>((set, get) => ({
   ...initialState,
 
   actions: {
-    fetchMeals: async (date, props: ActionProps) => {
-      props?.onStart?.();
-
-      set({ selectedDate: date, loading: true });
-
-      try {
-        const meals = await api.fetchMeals(date);
-        set({ meals });
-        props?.onSuccess?.();
-      } catch (error) {
-        set({ error: error.message });
-        console.error(error);
-        props?.onError?.(error.message);
-      } finally {
-        set({ loading: false });
-        props?.onFinally?.();
-      }
-    },
-
     prevDay: () => {
       const date = new Date(get().selectedDate);
       date.setDate(date.getDate() - 1);
       const dateString = date.toISOString().split("T")[0];
 
       set({ selectedDate: dateString });
-      get().actions.fetchMeals(dateString);
     },
 
     nextDay: () => {
@@ -50,66 +24,6 @@ const useMealsStore = create<MealStoreState>((set, get) => ({
       const dateString = date.toISOString().split("T")[0];
 
       set({ selectedDate: dateString });
-      get().actions.fetchMeals(dateString);
-    },
-
-    addMeals: async (newMeals: MealUI[], props: ActionProps) => {
-      props?.onStart?.();
-
-      // optimistic update
-      set((state) => ({
-        pendingMeals: [...state.pendingMeals, ...newMeals],
-      }));
-
-      try {
-        const mappedMeals = newMeals.map((meal: MealUI) => ({
-          food_id: meal.food.id,
-          quantity: meal.quantity,
-        }));
-        await api.addMeals(mappedMeals);
-        props?.onSuccess?.();
-
-        // fetch meals again to get the latest data
-        get().actions.fetchMeals(get().selectedDate, {
-          onFinally: () => {
-            // remove the meals from pending meals
-            set((state) => ({
-              pendingMeals: state.pendingMeals.filter(
-                (meal) =>
-                  !newMeals.some((newMeal) => newMeal.food === meal.food)
-              ),
-            }));
-          },
-        });
-      } catch (error) {
-        set({ error: error.message });
-        console.error(error);
-        props?.onError?.(error.message);
-      } finally {
-        // set({ loading: false });
-        props?.onFinally?.();
-      }
-    },
-
-    removeMeal: async (id: string, props: ActionProps) => {
-      props?.onStart?.();
-
-      set({ loading: true });
-
-      try {
-        await api.removeMeal(id);
-
-        props?.onSuccess?.();
-
-        get().actions.fetchMeals(get().selectedDate);
-      } catch (error) {
-        set({ error: error.message });
-        console.error(error);
-        props?.onError?.(error.message);
-      } finally {
-        set({ loading: false });
-        props?.onFinally?.();
-      }
     },
   },
 }));
--- client/stores/meals/types.ts
+++ client/stores/meals/types.ts
@@ -1,28 +1,11 @@
-import { Meal, MealUI } from "../../types/meals";
-import { Food } from "../../types/foods";
-
 interface MealStoreProperties {
   selectedDate: string | null;
-  meals: Meal[];
-  pendingMeals: MealUI[];
-  loading: boolean;
-  error: string | null;
-}
-
-export interface ActionProps {
-  onStart?: () => void;
-  onSuccess?: () => void;
-  onError?: (error: string) => void;
-  onFinally?: () => void;
 }
 
 interface MealStoreActions {
   actions: {
-    fetchMeals: (date: string, props?: ActionProps) => void;
     prevDay: () => void;
     nextDay: () => void;
-    addMeals: (newMeals: MealUI[], props?: ActionProps) => void;
-    removeMeal: (id: string, props?: ActionProps) => void;
   };
 }
 
